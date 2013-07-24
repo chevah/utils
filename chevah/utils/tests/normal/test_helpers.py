ea1cb38c1ce54b525a7ddba8eb9398daf5c26c21
bnickname <bnickname@chevah.com>
2013-07-24 10:37:18 +0300

    Added more tests.

--- chevah/utils/tests/normal/test_helpers.py
+++ chevah/utils/tests/normal/test_helpers.py
@@ -103,3 +103,81 @@ class TestHelpers(UtilsTestCase):
             message,
             )
         self.assertEqual(0, exit_code)
+
+    def test_generate_ssh_key_with_comment(self):
+        """
+        When a comment is provided, it will be added to the public key file.
+        """
+        options = self.Bunch(
+            key_size=1024,
+            key_type=u'RSA',
+            key_file=None,
+            key_comment=u'this is a comment',
+            )
+        key = DummyKey()
+        open_method = DummyOpenContext()
+
+        exit_code, message = generate_ssh_key(
+            options, key=key, open_method=open_method)
+
+        # Writes the public key.
+        self.assertEqual(
+            {'path': 'id_rsa.pub', 'mode': 'wb'}, open_method.calls[1])
+        self.assertContains(u'having comment "this is a comment"', message)
+        self.assertEqual(0, exit_code)
+
+    def test_generate_ssh_key_dsa(self):
+        """
+        When key type is DSA, it will generate DSA key.
+        """
+        options = self.Bunch(
+            key_size=1024,
+            key_type=u'DSA',
+            key_file=None,
+            key_comment=None,
+            )
+        key = DummyKey()
+        open_method = DummyOpenContext()
+
+        exit_code, message = generate_ssh_key(
+            options, key=key, open_method=open_method)
+
+        # Key is generated with requested arguments.
+        key.generate.assert_called_once_with(
+            key_type=crypto.TYPE_DSA, key_size=1024)
+        # Both private and public keys are stored.
+        self.assertEqual(2, key.store.call_count)
+        # First is writes the private key.
+        self.assertEqual(
+            {'path': 'id_dsa', 'mode': 'wb'}, open_method.calls[0])
+        # Then it writes the public key.
+        self.assertEqual(
+            {'path': 'id_dsa.pub', 'mode': 'wb'}, open_method.calls[1])
+        self.assertContains(u'SSH key of type "DSA"', message)
+        self.assertEqual(0, exit_code)
+
+    def test_generate_ssh_key_file(self):
+        """
+        When a file name is provided, the key is generated having this name.
+        """
+        options = self.Bunch(
+            key_size=1024,
+            key_type=u'RSA',
+            key_file=u'test_file',
+            key_comment=None,
+            )
+        key = DummyKey()
+        open_method = DummyOpenContext()
+
+        exit_code, message = generate_ssh_key(
+            options, key=key, open_method=open_method)
+
+        # First is writes the private key.
+        self.assertEqual(
+            {'path': 'test_file', 'mode': 'wb'}, open_method.calls[0])
+        # Then it writes the public key.
+        self.assertEqual(
+            {'path': 'test_file.pub', 'mode': 'wb'}, open_method.calls[1])
+        self.assertContains(u'public key file "test_file.pub"', message)
+        self.assertContains(u'private key file "test_file"', message)
+        self.assertEqual(0, exit_code)
