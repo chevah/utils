d553c626cb03a3ad1ce33c3abff3dd778e66e9d7
Adi Roiban <adi.roiban@chevah.com>
2013-07-30 23:15:22 +0300

    Update tests.

--- chevah/utils/tests/normal/system/test_helpers.py
+++ chevah/utils/tests/normal/system/test_helpers.py
@@ -5,6 +5,7 @@ System tests for helpers.
 """
 from nose.plugins.attrib import attr
 
+from chevah.utils.crypto import Key
 from chevah.utils.helpers import (
     generate_ssh_key,
     )
@@ -13,16 +14,17 @@ from chevah.utils.testing import mk, UtilsTestCase
 
 class TestHelpers(UtilsTestCase):
     """
-    Tests for helpers.
+    System tests for helpers.
     """
 
     @attr('slow')
-    def test_generate_ssh_key(self):
+    def test_generate_ssh_key_pair(self):
         """
-        Private/public key pair is generated, stored in the specified files
-        and are of desired type/length.
+        Private key pair is generated at specified path, and the public
+        key is generated on a path based on private key path.
         """
-        file_path, private_segments = mk.fs.makePathInTemp()
+        private_path, private_segments = mk.fs.makePathInTemp()
+        public_path = u'%s.pub' % (private_path)
         public_segments = private_segments[:]
         public_segments[-1] = u'%s.pub' % (public_segments[-1])
         comment = u'%s %s' % (mk.string(), mk.string())
@@ -32,21 +34,35 @@ class TestHelpers(UtilsTestCase):
         options = self.Bunch(
             key_size=1024,
             key_type='rsa',
-            key_file=file_path,
+            key_file=private_path,
             key_comment=comment,
             )
         self.assertFalse(mk.fs.exists(private_segments))
         self.assertFalse(mk.fs.exists(public_segments))
 
         try:
-            exit_code, message = generate_ssh_key(options)
+            generate_ssh_key(options)
 
             self.assertTrue(mk.fs.exists(private_segments))
             self.assertTrue(mk.fs.exists(public_segments))
+            # Check content on private key.
+            private_key = Key.fromFile(filename=private_path.encode('utf-8'))
+            self.assertEqual(1024, private_key.size)
+            self.assertIsFalse(private_key.isPublic())
+            self.assertEqual('RSA', private_key.type())
+
+            # Check content of public key.
+            public_key = Key.fromFile(filename=public_path.encode('utf-8'))
+            self.assertEqual(1024, public_key.size)
+            self.assertIsTrue(public_key.isPublic())
+            self.assertEqual('RSA', public_key.type())
+
+            # Check that public key is the pair of private key.
+            private_data = private_key.data()
+            public_data = public_key.data()
+            self.assertEqual(private_data['e'], public_data['e'])
+            self.assertEqual(private_data['n'], public_data['n'])
+
         finally:
             mk.fs.deleteFile(private_segments)
             mk.fs.deleteFile(public_segments)
-        self.assertContains('type "rsa"',  message)
-        self.assertContains('length "1024"', message)
-        self.assertContains('having comment "%s"' % comment, message)
-        self.assertEqual(0, exit_code)
--- chevah/utils/tests/normal/test_helpers.py
+++ chevah/utils/tests/normal/test_helpers.py
@@ -46,7 +46,7 @@ class DummyOpenContext(object):
 
 class TestHelpers(UtilsTestCase):
     """
-    Tests for helpers.
+    Unit tests for helpers.
     """
 
     def test_import_as_string_error(self):
@@ -63,45 +63,6 @@ class TestHelpers(UtilsTestCase):
         module = import_as_string('chevah.utils')
 
         self.assertEqual('chevah.utils', module.__name__)
-
-    def test_generate_ssh_key_default_values(self):
-        """
-        When no file and no comment is provided, it will use default
-        values.
-        """
-        options = self.Bunch(
-            key_size=1024,
-            key_type=u'RSA',
-            key_file=None,
-            key_comment=None,
-            )
-        key = DummyKey()
-        open_method = DummyOpenContext()
-
-        exit_code, message = generate_ssh_key(
-            options, key=key, open_method=open_method)
-
-        # Key is generated with requested arguments.
-        key.generate.assert_called_once_with(
-            key_type=crypto.TYPE_RSA, key_size=1024)
-        # Both private and public keys are stored.
-        key.store.assert_has_calls([
-            call(private_file=open_method),
-            call(public_file=open_method, comment=None),
-            ])
-        # First is writes the private key.
-        self.assertEqual(
-            {'path': 'id_rsa', 'mode': 'wb'}, open_method.calls[0])
-        # Then it writes the public key.
-        self.assertEqual(
-            {'path': 'id_rsa.pub', 'mode': 'wb'}, open_method.calls[1])
-        self.assertEqual(
-            u'SSH key of type "RSA" and length "1024" generated as public '
-            u'key file "id_rsa.pub" and private key file "id_rsa" without '
-            u'a comment.',
-            message,
-            )
-        self.assertEqual(0, exit_code)
 
     def test_generate_ssh_key_custom_values(self):
         """
@@ -120,15 +81,16 @@ class TestHelpers(UtilsTestCase):
         exit_code, message = generate_ssh_key(
             options, key=key, open_method=open_method)
 
-        # Key is generated with requested arguments.
+        # Key is generated with requested type and length.
         key.generate.assert_called_once_with(
             key_type=crypto.TYPE_DSA, key_size=2048)
-        # Both private and public keys are stored.
+        # Both private key and public key with comments are stored.
+		self.assertEqual(2, key.store.call_count)
         key.store.assert_has_calls([
             call(private_file=open_method),
             call(public_file=open_method, comment=u'this is a comment'),
             ])
-        # First is writes the private key.
+        # First it writes the private key.
         self.assertEqual(
             {'path': 'test_file', 'mode': 'wb'}, open_method.calls[0])
         # Then it writes the public key.
@@ -141,3 +103,38 @@ class TestHelpers(UtilsTestCase):
             message,
             )
         self.assertEqual(0, exit_code)
+
+    def test_generate_ssh_key_default_values(self):
+        """
+        When no path and no comment are provided, it will use default
+        values.
+        """
+        options = self.Bunch(
+            key_size=1024,
+            key_type=u'RSA',
+            key_file=None,
+            key_comment=None,
+            )
+        key = DummyKey()
+        open_method = DummyOpenContext()
+
+        exit_code, message = generate_ssh_key(
+            options, key=key, open_method=open_method)
+
+        # Writes private key and public key without a comment.
+        key.store.assert_has_calls([
+            call(private_file=open_method),
+            call(public_file=open_method, comment=None),
+            ])
+        # Default file path is used for private and public keys.
+        self.assertEqual(
+            {'path': 'id_rsa', 'mode': 'wb'}, open_method.calls[0])
+        self.assertEqual(
+            {'path': 'id_rsa.pub', 'mode': 'wb'}, open_method.calls[1])
+        # Message inform what default values were used.
+        self.assertEqual(
+            u'SSH key of type "RSA" and length "1024" generated as public '
+            u'key file "id_rsa.pub" and private key file "id_rsa" without '
+            u'a comment.',
+            message,
+            )
